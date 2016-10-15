839f008dcb94198f1d898c73830d7aef4a5e6d78
Austin Lanari & Jack Feely <pair+foggy1.jackfeely@devbootcamp.com>
2016-10-15 17:03:03 -0400

    move title of bill jsx file to <p> tag indiv formatting css

--- app/assets/javascripts/components/bill.es6.jsx
+++ app/assets/javascripts/components/bill.es6.jsx
@@ -2,26 +2,25 @@ class Bill extends React.Component {
 
   constructor() {
     super();
-    // this.addChart = this.addChart.bind(this);
-    // this.removeChart = this.removeChart.bind(this);
+    this.addChart = this.addChart.bind(this);
+    this.removeChart = this.removeChart.bind(this);
   }
 
-  // addChart() {
-  //   console.log("mouse entered");
-  //   addZeChart(this.props.supaKey);
-  // }
-  //
-  // removeChart() {
-  //   console.log("mouse left");
-  //   var chart = document.getElementById(this.props.supaKey);
-  //   chart.remove();
-  //   var billID = this.props.othaSupaKey;
-  //   var chartID = this.props.supaKey;
-  //   $('#'+billID).append('<div id='+chartID+'></div>')
-  //   // debugger;
-  // }
-  //
-  //
+  addChart() {
+    console.log("mouse entered");
+    addZeChart(this.props.supaKey);
+  }
+
+  removeChart() {
+    console.log("mouse left");
+    var chart = document.getElementById(this.props.supaKey);
+    chart.remove();
+    var billID = this.props.othaSupaKey;
+    var chartID = this.props.supaKey;
+    $('#'+billID).append('<div id='+chartID+'></div>')
+    // debugger;
+  }
+
   // <li id={this.props.othaSupaKey}>
   //   <div onMouseEnter={this.addChart} onMouseLeave={this.removeChart}>
   //     <time>{this.props.data.date}</time> {this.props.data.descrip}
@@ -37,7 +36,11 @@ class Bill extends React.Component {
     let {supaKey, othaSupaKey} = this.props
     return(
       <li id={othaSupaKey}>
-        <div><time>{year}</time> {title}</div>
+        <div onMouseEnter={this.addChart} onMouseLeave={this.removeChart}>
+          <time>{year}</time>
+          <p>{title}</p>
+        </div>
+        <div id={supaKey}></div>
       </li>
 
     )
